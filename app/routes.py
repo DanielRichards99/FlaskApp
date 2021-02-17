0dc1b136705972e08cedfd32097da5152ff2ef74
Daniel Richards <daniel.richards@landregistry.gov.uk>
2021-02-17 10:51:00 +0000

    Completed chapter 2 - Templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'Daniel'}
+    posts = [
+        {
+            'author': {'username': 'Liam'},
+            'body': 'Good day today!'
+        },
+        {
+            'author': {'username': 'Andrew'},
+            'body': 'Last Jedi was a bad movie!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
