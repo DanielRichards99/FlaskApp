05d6d5ad16fda52d69e47c3af9bc82fb8df95086
Daniel Richards <daniel.richards@landregistry.gov.uk>
2021-02-18 11:45:24 +0000

    Added gravatar avatar templates for user profile pictures

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from flask_login import UserMixin
+from hashlib import md5
 from app import db
 from app import login
 
@@ -24,6 +25,11 @@ class User(UserMixin, db.Model):
     @login.user_loader
     def load_user(id):
         return User.query.get(int(id))
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 
 class Post(db.Model):
