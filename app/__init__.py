44fedbc5aeab2f43f2a90feff8e85913c0b027b5
Daniel Richards <daniel.richards@landregistry.gov.uk>
2021-02-19 12:19:40 +0000

    Added bootstrap navbar

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,6 +17,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
@@ -45,3 +48,5 @@ if not app.debug:
 
         app.logger.setLevel(logging.INFO)
         app.logger.info('Microblog startup')
+
+mail = Mail(app)
