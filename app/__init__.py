21fec89dd3b44e4956cb7112710f9e391b915b98
Daniel Richards <daniel.richards@landregistry.gov.uk>
2021-02-23 15:23:11 +0000

    Added error handling blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -26,7 +26,10 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 babel = Babel(app)
 
-from app import routes, models, errors
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
+
+from app import routes, models
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -60,5 +63,4 @@ mail = Mail(app)
 
 @babel.localeselector
 def get_locale():
-    # return request.accept_languages.best_match(app.config['LANGUAGES'])
-    return 'es'
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
